f0f7d3ee9af46b056af63c8218f7a18917ef519a
David Eccles <36290667+Speccles96@users.noreply.github.com>
2022-03-27 01:21:03 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,19 +1,4 @@
-# Load packages ----
-require(shiny)
-require(shinythemes)
-require(maps)
-require(mapproj)
-require(ggplot2)
-require(sf)
 
-# Load data ----
-RQ_df <- readRDS("GeoToxHazard-App/nata_tox21_sp.rds")
-assay_list <-read.csv("GeoToxHazard-App/assay_list.csv")
-assay_list <- as.list(assay_list)
-states <- st_as_sf(maps::map("state", plot = FALSE, fill = TRUE))
-heatmap_df <- read.csv("GeoToxHazard-App/heatmap_df.csv")
-#chem_count_df <- readRDS("GeoToxHazard-App/chem_count_sp.rds")
-localG_df <- readRDS("GeoToxHazard-App/localG_sp.rds")
 
 # User interface ----
 ui <- fluidPage(theme = shinytheme("flatly"),
